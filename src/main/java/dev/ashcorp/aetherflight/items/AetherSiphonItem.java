09530777d2e22a90c2f3fbb1029f39c46b0cce7a
Ashen <ashen@ashen.live>
2022-01-25 09:59:21 +0000

    Added basic tooltip info to item.

--- src/main/java/dev/ashcorp/aetherflight/items/AetherSiphonItem.java
+++ src/main/java/dev/ashcorp/aetherflight/items/AetherSiphonItem.java
@@ -1,16 +1,32 @@
 package dev.ashcorp.aetherflight.items;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.UUID;
 
 import static dev.ashcorp.aetherflight.setup.ModSetup.ITEM_GROUP;
 
 public class AetherSiphonItem extends Item {
 
+    private static final Logger LOGGER = LogManager.getLogger();
+
     private int tier = 0;
-    private double storedAether = 0f;
-    private double maxAether = 10f;
+    private int storedAether = 0;
+    private int maxAether = 5000;
+    private UUID owner = null;
 
     public AetherSiphonItem(Properties pProperties) {
         super(pProperties);
@@ -18,6 +34,29 @@ public class AetherSiphonItem extends Item {
         pProperties.tab(ITEM_GROUP);
         pProperties.rarity(Rarity.COMMON);
         pProperties.stacksTo(1);
+    }
+
+    @Override
+    public void onUseTick(Level pLevel, LivingEntity pLivingEntity, ItemStack pStack, int pRemainingUseDuration) {
+        if (this.owner == null) {
+            setOwner(pLivingEntity.getUUID());
+        } else if (getOwner() != null && pLivingEntity.getUUID() != getOwner()) {
+            // Entity trying to use the item isn't the item owner.
+            LOGGER.info("User is not the item owner");
+        } else {
+            // Do the things.
+            int stored = getStoredAether();
+            setStoredAether(stored + 10);
+        }
+        super.onUseTick(pLevel, pLivingEntity, pStack, pRemainingUseDuration);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(new TextComponent(String.format("Tier: %s", tier)));
+        pTooltipComponents.add(new TextComponent(String.format("Stored Aether: %s / %s", storedAether, maxAether)));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+
     }
 
     public int getTier() {
@@ -28,19 +67,27 @@ public class AetherSiphonItem extends Item {
         this.tier = tier;
     }
 
-    public double getStoredAether() {
+    public int getStoredAether() {
         return this.storedAether;
     }
 
-    public void setStoredAether(double storedAether) {
+    public void setStoredAether(int storedAether) {
         this.storedAether = storedAether;
     }
 
-    public double getMaxAether() {
+    public int getMaxAether() {
         return this.maxAether;
     }
 
-    public void setMaxAether(double maxAether) {
+    public void setMaxAether(int maxAether) {
         this.maxAether = maxAether;
+    }
+
+    public UUID getOwner() {
+        return this.owner;
+    }
+
+    public void setOwner(UUID owner) {
+        this.owner = owner;
     }
 }
